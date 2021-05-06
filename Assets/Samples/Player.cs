9362c0c81f5ca879027f0ec743504e9ee45efa44
Jeffrey Meldgin <jmeldgin@gmail.com>
2021-05-05 19:36:56 -0700

    Add Pixel Summary Asset Pack

---
+++ Assets/Samples/Player.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    //Config
+    [SerializeField] float walkSpeed = 5f;
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float climbSpeed = 3f;
+    [SerializeField] float crouchSpeed = 3f;
+    [SerializeField] bool isCrouching;
+    [SerializeField] bool isWalking;
+    [SerializeField] bool freezeCharacter;
+    
+
+    //Cached Component References
+    Rigidbody myRigidbody;
+    Animator myAnimator;
+    BoxCollider myBoxCollider;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myAnimator = GetComponent<Animator>();
+        myBoxCollider = GetComponent<BoxCollider>();
+        isCrouching = myAnimator.GetBool("Crouching");
+    }
+
+    void Update()
+    {
+        Walk();
+        Crouch();
+        FlipSprite();
+        CharacterCantMove();
+        print(myRigidbody.velocity.x);
+    }
+    public void CharacterCantMove()
+    {
+        if (!freezeCharacter)
+        {
+            myRigidbody.constraints = RigidbodyConstraints.None;
+            myRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+        else if (freezeCharacter)
+        {
+            myRigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+    private void Walk()
+    {
+        if (!isCrouching) 
+        {
+            // X Axis Movement
+            float horizontalinputspeed = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+            Vector2 playerVelocity = new Vector2(horizontalinputspeed * walkSpeed, myRigidbody.velocity.y); // controlling speed of character
+            myRigidbody.velocity = playerVelocity; // actually making character move based on above line
+            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // bool deciding if velocity is higher than 0
+            myAnimator.SetBool("New Walking", playerHasHorizontalSpeed); // Toggling on the bool if the character is walking and turning it off if he's not
+
+
+            // Z Axis Movement
+            float verticalinputspeed = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector3 playerZVelocity = new Vector3(myRigidbody.velocity.x, myRigidbody.velocity.y, verticalinputspeed * walkSpeed);
+            myRigidbody.velocity = playerZVelocity;
+        }
+        else if (isCrouching)
+        {
+            CrouchWalking();
+        }
+    }
+    private void Crouch()
+    {
+        // Animation tells isCrouching = true & freezes character until after animation completes
+        if (Input.GetKeyDown(KeyCode.C) && (!isCrouching))
+        {
+            print("I've pressed C");
+            myAnimator.SetBool("Crouching", true);
+        }
+        // Else if I press C and I am crouching
+        else if (Input.GetKeyDown(KeyCode.C) && (isCrouching))
+        {
+            print("I've pressed C again");
+            myAnimator.SetBool("Crouching", false);
+        }
+    }
+    private void CrouchWalking()
+    {
+        if (isCrouching)
+        {
+            // X Axis Crouching
+            float horizontalinputspeed = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+            Vector2 playerVelocity = new Vector2(horizontalinputspeed * crouchSpeed, myRigidbody.velocity.y); // controlling speed of character
+            myRigidbody.velocity = playerVelocity; // actually making character move based on above line
+
+            bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // bool deciding if velocity is higher than 0
+            myAnimator.SetBool("CrouchWalking", playerHasHorizontalSpeed);
+            // Z Axis Crouching
+            float verticalinputspeed = CrossPlatformInputManager.GetAxis("Vertical");
+            Vector3 playerZVelocity = new Vector3(myRigidbody.velocity.x, myRigidbody.velocity.y, verticalinputspeed * crouchSpeed);
+            myRigidbody.velocity = playerZVelocity;
+        }
+    }
+    private void FlipSprite()
+    {
+        bool whichDirectionPlayerFacing = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (whichDirectionPlayerFacing)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(myRigidbody.velocity.x), 1f, 1f);
+        }
+    }
+}
+
